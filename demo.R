273482518388326dcdd59f6aa6e5ad9200ef253a
Chester Wang <gengxingwang2019@u.northwestern.edu>
2022-11-17 16:54:16 -0800

    Update demo.R

--- demo.R
+++ demo.R
@@ -11,7 +11,13 @@ Options:
 [--arg4=<arg4>]   Takes any value (this is an optional option)
 " -> doc
 
-library(docopt)
-opt <- docopt(doc)
-print(opt)
-print(typeof(opt))
+main <- function() {
+    library(docopt)
+    opt <- docopt(doc)
+    print(opt)
+    print(typeof(opt))
+}
+
+main()
+
+
